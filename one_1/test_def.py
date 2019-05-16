eb14d79609e50084c053f05ebcdd862a3af6d9c5
DSW1609 <DSW7296666@163.com>
2019-05-16 11:22:09 +0800

    5-16

--- one_1/test_def.py
+++ one_1/test_def.py
@@ -9,7 +9,7 @@
 #             return True
 # x = int(input("请输入一个数:"))
 # print(sushu(x))
-
+# 
 # def sushu(x):
 #     #100-200之间所有的素数
 #         for y in range(2,x):
@@ -22,10 +22,10 @@
 #     if sushu(x) == True:
 #         L.append(x)
 # print(L)
-
+# 
 # i = input("请输入一组数字 用空格隔开：")
 # L = [int(x) for x in i.split()]
-# def pjz(L):
+# def pjz(ave):
 #     #一组数平均值
 #     sum = 0
 #     for y in L:
@@ -33,7 +33,7 @@
 #         ave = sum/len(L)
 #     return ave
 # print(pjz(L))
-
+# 
 # def zys(n):#求正整数的质因数
 #     L = set([1])
 #     while n > 1:
@@ -47,14 +47,36 @@
 #     s = int(input('请输入一个正整数：'))
 #     print("质因数："+' '.join([str(x) for x in zys(s)]))
 #     break;
-L = [1,2,3,4,5]    # 列表的循环左移K位
-k = int(input("请输入往左移几位？"))
-def zy(L):
-    n = 0
-    for i in range(k):
-        L.append(L[n])
-        n+=1
-    for i in range(k):
-        L.pop(0)
-    return L
-print(zy(L))
+# L = [1,2,3,4,5]    # 列表的循环左移K位
+# k = int(input("请输入往左移几位？"))
+# def zy(L):
+#     n = 0
+#     for i in range(k):
+#         L.append(L[n])
+#         n+=1
+#     for i in range(k):
+#         L.pop(0)
+#     return L
+# print(zy(L))
+
+# def huiwen(x):  # 判断一个整数是不是回文数
+#     for i in range(int(len(x)/2)):
+#         if x[i] == x[-(i+1)]:
+#             return True
+#         else:
+#             return False
+# x = input("请输入一个整数：")
+# print(huiwen(x))
+
+# def jch(n):     # 求阶乘
+#     sum = 0
+#     m = 1
+#     for i in range(1,n+1):
+#         m *= i
+#         sum += m
+#     return sum
+# n = int(input("请输入n的值："))
+# print(jch(n))
+
+def sxh(n):
+    if n[0]
